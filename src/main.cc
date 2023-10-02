e95d25458af3f3d707916bad7d6f1d8f107a4f4a
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-10-01 21:47:21 -0700

    feat: add compile line command options

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <deque>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include "src/parser.hh"
+#include "src/lexer.hh"
+#include "src/llvm.hh"
+
+int main(int argc, char *argv[])
+{
+    std::string fileName = std::string(argv[1]);
+
+    std::ifstream t("/Users/codyzeitler/dev/anchor/" + fileName);
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+
+    std::string a = buffer.str();
+
+    std::deque<lexer::Token> tokens = lexer::lex(a);
+    parser::Parser frontEnd(tokens);
+    parser::Program program = frontEnd.parse();
+
+    llvm::Parser llvmGenerator(program);
+    llvm::Program instructions = llvmGenerator.parse();
+
+    llvm::Writer writer;
+    std::string output = writer.write(instructions);
+
+    std::ofstream compiledLlvmFile;
+    compiledLlvmFile.open("/Users/codyzeitler/dev/anchor/" + fileName + ".ll");
+    compiledLlvmFile << output;
+    compiledLlvmFile.close();
+
     return 0;
 }
---
+++ test.anchor.ll
@@ -0,0 +1,8 @@
+define i32 @foo() #0 {
+  %1 = add i32 3, 5
+  ret i32 %1
+}
+define i32 @main() #0 {
+  %1 = add i32 0, 0
+  ret i32 %1
+}
