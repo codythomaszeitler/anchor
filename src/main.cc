a5c0d0b869a08879e11f90a1a7df8c0f8e687a8e
Cody T. Zeitler <codyzeitler12@gmail.com>
2023-10-05 19:27:08 -0700

    feat: compile with llvm

--- src/main.cc
+++ src/main.cc
@@ -1,38 +1,20 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include <deque>
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include <memory>
 
-#include "src/parser.hh"
-#include "src/lexer.hh"
-#include "src/llvm.hh"
+using namespace llvm;
 
 int main(int argc, char *argv[])
 {
-    std::string fileName = std::string(argv[1]);
+    std::unique_ptr<LLVMContext> TheContext;
+    std::unique_ptr<Module> TheModule;
+    std::unique_ptr<IRBuilder<>> Builder;
+    TheContext = std::make_unique<LLVMContext>();
+    TheModule = std::make_unique<Module>("my cool jit", *TheContext);
 
-    std::ifstream t("/Users/codyzeitler/dev/anchor/" + fileName);
-    std::stringstream buffer;
-    buffer << t.rdbuf();
+    // Create a new builder for the module.
+    Builder = std::make_unique<IRBuilder<>>(*TheContext);
 
-    std::string a = buffer.str();
-
-    std::deque<lexer::Token> tokens = lexer::lex(a);
-    parser::Parser frontEnd(tokens);
-    parser::Program program = frontEnd.parse();
-
-    llvm::Parser llvmGenerator(program);
-    llvm::Program instructions = llvmGenerator.parse();
-
-    llvm::Writer writer;
-    std::string output = writer.write(instructions);
-
-    std::ofstream compiledLlvmFile;
-    compiledLlvmFile.open("/Users/codyzeitler/dev/anchor/" + fileName + ".ll");
-    compiledLlvmFile << output;
-    compiledLlvmFile.close();
-
-    return 0;
+    return 1;
 }
